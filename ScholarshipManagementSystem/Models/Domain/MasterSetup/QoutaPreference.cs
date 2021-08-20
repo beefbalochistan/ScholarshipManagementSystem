69ab477c0bc408c3e50b92d7e79d0c22592706d8
Dot Net Developer <kafil.fast09@gmail.com>
2021-08-20 02:37:08 -0700

    Role Permission

---
+++ ScholarshipManagementSystem/Models/Domain/MasterSetup/QoutaPreference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScholarshipManagementSystem.Models.Domain.MasterSetup
+{
+    public class QoutaPreference
+    {
+        public int QoutaMetric { get; set; }
+        public int QoutaFAFSc1Y { get; set; }
+        public int QoutaFAFSc2Y { get; set; }
+        public int QoutaDAE1Y { get; set; }
+        public int QoutaDAE2Y { get; set; }
+        public int QoutaDAE3Y { get; set; }
+        public int QoutaBacholar1Y { get; set; }
+        public int BacholarQouta { get; set; }
+        public int MasterQouta { get; set; }
+    }
+}
